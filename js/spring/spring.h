14a6eaef4bd3b9e4d58a879a204e3d4de4baed50
D4 <d4@thegrid.io>
2022-02-14 20:59:19 -0800

    fixed

---
+++ js/spring/spring.h
@@ -0,0 +1,7 @@
+#include <math.h> // https://www.lemoda.net/c/maths-constants/
+
+
+// double exp(double x); // returns e raised to the power of x
+// double pow(double x, double y);
+// double sqrt(double x);
+// double sinh(double x);
